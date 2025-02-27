5122e3a102b26fa86b6894770a51e84128bc00eb
Wolfgang Reuter <wolfgang.reuter@alexanderthamm.com>
2025-02-27 15:36:02 +0100

    system prompt added

--- src/csv_agent_1_demo.py
+++ src/csv_agent_1_demo.py
@@ -28,15 +28,12 @@ from pathlib import Path
 
 from langchain_experimental.agents import create_csv_agent 
 from langchain_openai import ChatOpenAI 
+from langchain.prompts import PromptTemplate
 
 st.set_page_config(page_title="Ask your CSV")
 
 # Get the current working directory (will be the project root in Streamlit Cloud)
 project_root = Path(os.getcwd()) 
-st.write(f"Current working directory: {project_root}")
-
-project_root = Path(__file__).resolve().parent.parent  # Go up one level
-st.write(f"Current working directory: {project_root}")
 
 # =============================================================================
 # Paths and Variables
@@ -49,6 +46,27 @@ IMAGE_PATH_GDP = project_root / "economic_growth.jpg"
 CSV_ID_CLIMATE = "GlobalLandTemperaturesByMajorCity.csv"
 CSV_ID_TITANIC = "titanic_1.csv"
 CSV_ID_GPD = "gdp.csv"
+
+agent_prompt = """
+You are an expert in statistics - and you also have a general broad knowledge. 
+You are absolutetely familiar with Python and pandas dataframes. You are 
+provided with some data - and when a user asks you a question, you analyze 
+the data and you try to find the answer. 
+When you analyze the data, you first of all look in the provided data. If you 
+can't analyze the data because there are NaN values in it, you may think about
+a reasonable way to replace or clean the data. If you have not enough 
+information within the dataset that you are provided with, you may look 
+elsewhere. In that case you mention in some way which information was not 
+available in the dataset you are provided with. 
+You always think about the question and how to analyze it first. If a user 
+question is unspecific or seems strange to you, 
+you **will ask the user what he or she means** by anserwing with a question. 
+You **formulate your questions in full sentences** and in a way 
+that the user understands what information with regards to the question you 
+need. 
+Your final answers are **also in full sentences**. You are polite - 
+but **also to the point**.'
+"""
 
 # =============================================================================
 # Functions
@@ -64,6 +82,14 @@ def main():
     # Display an image
     image_placeholder.image(IMAGE_PATH_TITANIC)
     
+    # Define the custom prompt with a system message
+    custom_prompt = PromptTemplate(
+            input_variables=["query"],
+            template=agent_prompt
+        )
+    
+    
+    
     user_csv = st.file_uploader("Upload your CSV file", type="csv")
     if user_csv is not None: 
         file_name = user_csv.name 
@@ -74,14 +100,21 @@ def main():
         else: 
             image_placeholder.image(IMAGE_PATH_TITANIC)
         user_question = st.text_input("Ask a question about your CSV: ")
-        llm = ChatOpenAI(temperature=0, openai_api_key=OPENAI_API_KEY)
-        agent = create_csv_agent(llm, user_csv, verbose=True, 
-                                 allow_dangerous_code=True)
+        llm = ChatOpenAI(model="gpt-4o", temperature=0, 
+                         openai_api_key=OPENAI_API_KEY)
+        # Create the agent with the custom prompt
+        agent = create_csv_agent(
+            llm, 
+            user_csv, 
+            verbose=True, 
+            allow_dangerous_code=True, 
+            prompt=custom_prompt
+        )
         if user_question is not None and user_question != "": 
             
-            response = agent.run(user_question)
+            response = agent.invoke(user_question)
             
-            st.write(response)
+            st.write(response["output"])
     
 
 # =============================================================================
